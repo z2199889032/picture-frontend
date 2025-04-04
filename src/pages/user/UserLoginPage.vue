0329a1ad862141c947a0bd606cf4eab83c61bbcd
zzm <2199889032@qq.com>
2025-04-04 21:06:38 +0800

    docs(user): 更新用户登录页面描述
    - 将用户登录页面的描述从"企业级智能协同云图库"修改为"企业级智能云图库"
    -简化了页面描述，使其更加简洁明了

--- src/pages/user/UserLoginPage.vue
+++ src/pages/user/UserLoginPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="userLoginPage">
     <h2 class="title">逸云图库 - 用户登录</h2>
-    <div class="desc">企业级智能协同云图库</div>
+    <div class="desc">企业级智能云图库</div>
     <a-form :model="formState" name="basic" autocomplete="off" @finish="handleSubmit">
       <a-form-item name="userAccount" :rules="[{ required: true, message: '请输入账号' }]">
         <a-input v-model:value="formState.userAccount" placeholder="请输入账号" />
